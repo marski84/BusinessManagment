862a384543fab167d2c1ec7e8d2d8cf18613457d
marski84 <marski705@gmail.com>
2024-01-14 14:32:15 +0100

    implemented spinner handling based on router events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule, RouterOutlet} from '@angular/router';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart, ResolveEnd, ResolveStart,
+  Router, RouterEvent,
+  RouterModule,
+  RouterOutlet
+} from '@angular/router';
 import {HttpClientModule} from "@angular/common/http";
 import {AuthModule} from "./modules/auth-module/auth.module";
 import {PanelModule} from "./modules/panel/panel.module";
@@ -8,6 +16,8 @@ import {LoginService} from "./modules/auth-module/login.service";
 import {LoginServiceRepository} from "./modules/auth-module/LoginServiceRepository";
 import {SpinnerService} from "./modules/spinner/spinner.service";
 import {SpinnerModule} from "./modules/spinner/spinner.module";
+import {tap} from "rxjs";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-root',
@@ -17,6 +27,37 @@ import {SpinnerModule} from "./modules/spinner/spinner.module";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  router = inject(Router);
+  spinnerService = inject(SpinnerService);
+  destroyRef = inject(DestroyRef);
   title = 'BusinessManagment';
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        tap((event: any) => this.handleEvent(event))
+      )
+      .subscribe()
+  }
+
+
+  private handleEvent(event: RouterEvent) {
+    if (event instanceof NavigationStart || event instanceof ResolveStart) {
+      this.spinnerService.show();
+    }
+    if (event instanceof NavigationEnd || event instanceof ResolveEnd) {
+      this.spinnerService.hide();
+    }
+
+    // Set loading state to false in both of the below events to hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      this.spinnerService.hide();
+    }
+    if (event instanceof NavigationError) {
+      this.spinnerService.hide();
+    }
+
+  }
 }
